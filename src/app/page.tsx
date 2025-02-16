3c875df871c40f1a53d526cb738028d337e5f8d8
Kamal Joshi <thisiskamaljoshi@gmail.com>
2025-02-16 14:01:42 +0530

    Tic Tac Toe Ui , With Clicks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,69 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  type UserSymbol = "X" | "O" | null;
+
+  type Cell = {
+    id: number;
+    row: number;
+    col: number;
+    isClicked: boolean;
+    userSymbol: UserSymbol;
+  };
+
+  const [board, setBoard] = useState<Cell[]>([
+    { id: 1, row: 1, col: 1, isClicked: false, userSymbol: null },
+    { id: 2, row: 1, col: 2, isClicked: false, userSymbol: null },
+    { id: 3, row: 1, col: 3, isClicked: false, userSymbol: null },
+    { id: 4, row: 2, col: 1, isClicked: false, userSymbol: null },
+    { id: 5, row: 2, col: 2, isClicked: false, userSymbol: null },
+    { id: 6, row: 2, col: 3, isClicked: false, userSymbol: null },
+    { id: 7, row: 3, col: 1, isClicked: false, userSymbol: null },
+    { id: 8, row: 3, col: 2, isClicked: false, userSymbol: null },
+    { id: 9, row: 3, col: 3, isClicked: false, userSymbol: null },
+  ]);
+
+  const [gameEntries,setGameEntries] = useState<string[]>([]);
+
+  function tickTacCellClickHandler(cell:Cell){    
+
+    const newBoard = [...board];
+
+    if(!cell.isClicked){
+      if(gameEntries.length %2 === 0){
+        newBoard[cell.id-1].userSymbol = "X";
+      }else{
+        newBoard[cell.id-1].userSymbol = "O";
+      }
+      newBoard[cell.id-1].isClicked = true;
+      setGameEntries((old)=>[...old,`${cell.row}${cell.col}`]);
+      setBoard(newBoard);
+    }else{
+      console.log("Already clicked");
+    }
+  }
+
+  console.log(gameEntries,"gameEntries");
+  console.log(board,"board");
+
+  return (
+    <div className="h-screen">
+      <div className="flex flex-col items-center gap-4 p-4 h-full">
+        <h1 className="text-2xl font-bold">Tic Tac Toe</h1>
+        <div className="grid grid-cols-3 gap-2">
+          {board.map((cell) => {
+            return (
+              <button
+                key={cell.id}
+                className="size-24 md:size-28 lg:size-32  flex items-center justify-center border border-gray-500 text-2xl font-bold hover:bg-gray-200"
+                onClick={()=>tickTacCellClickHandler(cell)}
+              >{cell.userSymbol}</button>
+            );
+          })}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
